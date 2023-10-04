e821a0cb1eb29bc754aef6f782c9aee2f6bec580
Wisdom Nwachukwu <wisdom@tm30.net>
2023-10-04 08:32:46 -0700

    pushed from mac air

--- rider_expert/lib/main.dart
+++ rider_expert/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:rider_expert/utils/colors.dart';
 import 'package:sizer/sizer.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// You can specify all Themes in each WidgetTheme settings ... AppBarTheme
 var kLightTheme= ThemeData(
   useMaterial3: true,
 brightness: Brightness.light,
@@ -13,45 +14,17 @@ colorScheme: ColorScheme.light(
   secondary: AppColors.oxBlood,
 )
 );
+
 var kDarkTheme= ThemeData(
     useMaterial3: true, 
     brightness: Brightness.dark,
     colorScheme: ColorScheme.dark(
       background: AppColors.black,
       primary: AppColors.white,
-      secondary: AppColors.oxBlood
-
+      secondary: AppColors.oxBlood,
     )
 );
 
-
-// var kLightColorScheme = const ColorScheme(
-//   brightness: Brightness.light,
-//   background: AppColors.white,
-//   onBackground: Color.fromARGB(253, 214, 214, 214),
-//   primary: AppColors.black,
-//   onPrimary: Colors.black,
-//   secondary: AppColors.oxBlood,
-//   onSecondary:  Color.fromARGB(255, 40, 10, 10),
-//   error: AppColors.red,
-//   onError:  Color.fromARGB(255, 241, 114, 110),
-//   surface: Color.fromARGB(255, 255, 255, 255),
-//   onSurface: Color.fromARGB(255, 153, 152, 152),
-//     );
-
-// var kDarkColorScheme = const ColorScheme(
-//       brightness: Brightness.dark,
-//   background: AppColors.black,
-//   onBackground: Color.fromARGB(253, 214, 214, 214),
-//   primary: AppColors.white,
-//   onPrimary: Colors.black,
-//   secondary: AppColors.oxBlood,
-//   onSecondary:  Color.fromARGB(255, 40, 10, 10),
-//   error: AppColors.red,
-//   onError:  Color.fromARGB(255, 241, 114, 110),
-//   surface: Color.fromARGB(255, 255, 255, 255),
-//   onSurface: Color.fromARGB(255, 153, 152, 152),
-//     );
 
 void main() {
   runApp(Sizer(builder: (context, orientation, deviceType) {
